ae1786c1de5672325e0cda37298de6c9aa83f74b
Cusnd <i@cusnd.com>
2024-01-15 09:57:09 +0800

    2024年1月15日09点56分

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修复。
-        System.out.print("Hello and welcome!");
+        System.out.print("Hello and welcome!\n");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 0; i <= 5; i++) {
             //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
             // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
             System.out.println("i = " + i);
